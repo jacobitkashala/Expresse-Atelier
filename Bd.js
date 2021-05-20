d050144af08a2ddf30a234572931ec40892a8860
jacobitkashala <jacobitkashala@gmail.com>
2021-05-20 12:20:31 +0200

    Feat:to get database

---
+++ Bd.js
@@ -0,0 +1,10 @@
+const Student = [
+    { id: 1, nom: "Kota", prenom: "Typescript" },
+    { id: 2, nom: "Buhendwa", prenom: "FrontMaster" },
+    { id: 3, nom: "Mianza", prenom: "Elle" },
+    { id: 4, nom: "salomo", prenom: "jouer" },
+    { id: 4, nom: "Zender", prenom: "Dev Senior" },
+    { id: 4, nom: "xend", prenom: "lolo" },
+]
+
+module.exports = Student;
---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const students = require("./Bd");
+const uuid = require("uuid")
+const app = express();
+/*
+npm init -y
+npm i express
+npm nodemon permet de ne plus lancer le server
+npm run le nom du script
+on utilise get de app pour manipuler le response et du requette
+
+*/
+
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, resp) => {
+    resp.send("<h1>Precisez l'information</h1>");
+    // resp.json(students);
+})
+
+app.get("/api/students", (req, resp) => {
+    //resp.send("<h1>KDA is the best academy</h1>");
+    resp.json(students);
+})
+
+app.get("/api/students/:id", (req, resp) => {
+        //resp.send("<h1>KDA is the best academy</h1>");
+        //resp.send(req.params.id)
+        //const student = students.find((student) => student.id == req.params.id)
+        const { id } = req.params.id
+        const studentIdFound = students.some(student => student.id === parseInt(id))
+        if (studentIdFound) {
+            const student = students.find((student) => student.id == id);
+        } else {
+            //  resp.status(404).json({ error: `No student id ${id}` })
+            resp.send("<h1>404 error</h1>");
+        }
+
+        resp.json(student);
+    })
+    //faire le post
+
+app.post("/api/students", (req, resp) => {
+    const newStudent = { id: 10, nom: "Kota", prenom: "Typescript" }
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Le server tourn est sur le port ${PORT}`)
+});
